47f9f849835b2f5f424437127c229df5046a739c
OllieRumbol <otgb97@gmail.com>
2020-08-25 19:32:52 +0100

    Creating models for a task and job

---
+++ ToDoListModels/Job.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasksModels
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Done { get; set; }
+
+        public Job(int Id, string Name, bool Done)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Done = Done;
+        }
+    }
+}
