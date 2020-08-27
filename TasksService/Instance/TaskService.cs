173ada141ee515fc2f799baea63aa7eb198f8842
OllieRumbol <otgb97@gmail.com>
2020-08-27 08:18:34 +0100

    Initial files

---
+++ TasksService/Instance/TaskService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TasksModels;
+using TasksService.Interface;
+
+namespace TasksService.Instance
+{
+    public class TaskService : ITaskService
+    {
+        public static List<Task> ToDoTasks = new List<Task>();
+
+        public static List<Task> InProgressTasks = new List<Task>();
+
+        public static List<Task> DoneTasks = new List<Task>();
+
+        public int getNextId(List<Task> tasks)
+        {
+            return tasks.Max(t => t.Id);
+        }
+    }
+}
---
+++ TasksService/Interface/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TasksModels;
+
+namespace TasksService.Interface
+{
+    public interface ITaskService
+    {
+        int getNextId(List<Task> tasks);
+    }
+}
