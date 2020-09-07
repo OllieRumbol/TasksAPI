d796ce6f8a523874a75175abee2fbbd8b9937084
OllieRumbol <otgb97@gmail.com>
2020-09-07 22:06:39 +0100

    Add DI

--- TasksAPI/Startup.cs
+++ TasksAPI/Startup.cs
@@ -10,8 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using TasksService.Instance;
-using TasksService.Interface;
 
 namespace ToDoListAPI
 {
@@ -27,8 +25,6 @@ namespace ToDoListAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITaskService, TaskService>();
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
