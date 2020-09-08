929de38e014db2094b71d27cb1371b9d67f6faff
OllieRumbol <otgb97@gmail.com>
2020-09-08 09:10:07 +0100

    Get endpoints

---
+++ TasksAPI/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TasksService.Interface;
+
+namespace TasksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private ITaskService service;
+
+        public TasksController(ITaskService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public JsonResult getTaskById()
+        {
+            return new JsonResult(service.getAllTasks());
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult getAllTasks(int id)
+        {
+            return new JsonResult(service.getTaskById(id));
+        }
+    }
+}
--- TasksAPI/Startup.cs
+++ TasksAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TasksService.Instance;
+using TasksService.Interface;
 
 namespace ToDoListAPI
 {
@@ -25,6 +27,8 @@ namespace ToDoListAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITaskService, TaskService>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
